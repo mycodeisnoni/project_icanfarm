a5293e677955deeadc9abcf0fc3246cf12977abd
SeongJoong <ksj960712@naver.com>
2023-04-27 17:55:28 +0900

    Refactor: refactor code before making thread
    Refactor for easier thread programming
    
    Related to: #S08P31A206-151

--- RPi_mqtt/mqtt_pub.cpp
+++ RPi_mqtt/mqtt_pub.cpp
@@ -8,27 +8,35 @@ const std::string SERVER_ADDRESS("tcp://k8a206.p.ssafy.io:3333");
 const std::string CLIENT_ID("RPi-publish");
 const std::string TOPIC("test");
 const int QOS = 1;
-const int DELAY_INTERVAL_SECONDS = 1;
 
-class callback : public virtual mqtt::callback,
-    public virtual mqtt::iaction_listener {
+class callback : public virtual mqtt::callback {
 public:
     void connection_lost(const std::string& cause) override {
         std::cout << "Connection lost: " << cause << std::endl;
     }
 
+    // 발행 작업이 성공적으로 전달되었는지 확인하기 위한 콜백함수 -> 필수 X
     void delivery_complete(mqtt::delivery_token_ptr token) override {
         std::cout << "Delivery complete for token: " << token->get_message_id() << std::endl;
     }
-
-    void on_failure(const mqtt::token& tok) override {
-        std::cout << "Failure: " << tok.get_message_id() << std::endl;
-    }
-
-    void on_success(const mqtt::token& tok) override {
-        std::cout << "Success: " << tok.get_message_id() << std::endl;
-    }
 };
+
+void publish(mqtt::async_client& client) {
+    while (true) {
+        std::string payload = "";
+        std::cin >> payload;
+        mqtt::message_ptr pubmsg = mqtt::make_message(TOPIC, payload);
+        pubmsg->set_qos(QOS);
+
+        try {
+            mqtt::token_ptr pubtok = client.publish(pubmsg);
+            pubtok->wait();
+        }
+        catch (const mqtt::exception& exc) {
+            std::cerr << "Error: " << exc.what() << std::endl;
+        }
+    }
+}
 
 int main(int argc, char* argv[]) {
     mqtt::async_client client(SERVER_ADDRESS, CLIENT_ID);
@@ -49,24 +57,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    int count = 0;
-    while (true) {
-        std::string payload = "";
-        std::cin >> payload;
-        mqtt::message_ptr pubmsg = mqtt::make_message(TOPIC, payload);
-        pubmsg->set_qos(QOS);
-
-        try {
-            mqtt::token_ptr pubtok = client.publish(pubmsg);
-            pubtok->wait();
-        }
-        catch (const mqtt::exception& exc) {
-            std::cerr << "Error: " << exc.what() << std::endl;
-        }
-
-//        std::this_thread::sleep_for(std::chrono::seconds(DELAY_INTERVAL_SECONDS));
-    }
+    publish(client);
 
     return 0;
 }
-
