194a1ef4979b5f22b8262f4270c85c07ff5e9adb
SeongJoong <ksj960712@naver.com>
2023-05-04 14:10:13 +0900

    Add: cmd input data
    Made cmd input into CAN frame data
    
    Related to: #S08P31A206-178

--- RPi_can/send_test.cpp
+++ RPi_can/send_test.cpp
@@ -14,7 +14,7 @@ int main()
     struct can_frame frame;
     struct ifreq ifr;
 
-    const char *ifname = "vcan0";
+    const char* ifname = "vcan0";
 
     // 소켓 생성
     if ((sock = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0)
@@ -23,7 +23,7 @@ int main()
         return 1;
     }
 
-    strcpy(ifr.ifr_name, ifname);    // CAN 인터페이스 이름 설정
+    strcpy(ifr.ifr_name, ifname); // CAN 인터페이스 이름 설정
     ioctl(sock, SIOCGIFINDEX, &ifr); // 소켓-네트워크 인터페이스 연결
 
     // 소켓 주소 구조체 초기화
@@ -31,25 +31,34 @@ int main()
     addr.can_ifindex = ifr.ifr_ifindex;
 
     // 소켓에 주소 바인딩
-    if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    if (bind(sock, (struct sockaddr*)&addr, sizeof(addr)) < 0)
     {
         perror("Error in socket bind");
         return 1;
     }
 
-    // CAN 프레임 설정
-    frame.can_id = 0x123; // 프레임 ID
-    frame.can_dlc = 2;    // 데이터 길이
-    frame.data[0] = 0x11; // 데이터
-    frame.data[1] = 0x22;
+    // 프레임 ID 설정
+    frame.can_id = 0x123;
+    char input[256];
 
-    // CAN 프레임 송신
-    int nbytes = write(sock, &frame, sizeof(struct can_frame));
-
-    if (nbytes != sizeof(struct can_frame))
+    while (true)
     {
-        perror("Error in socket write");
-        return 1;
+        std::cin.getline(input, 256);
+
+        if (strcmp(input, "exit") == 0) break;
+
+        int input_len = strlen(input);
+        frame.can_dlc = std::min(input_len, 8);
+        memcpy(frame.data, input, frame.can_dlc);
+
+        // CAN 프레임 송신
+        int nbytes = write(sock, &frame, sizeof(struct can_frame));
+
+        if (nbytes != sizeof(struct can_frame))
+        {
+            perror("Error in socket write");
+            return 1;
+        }
     }
 
     close(sock);
