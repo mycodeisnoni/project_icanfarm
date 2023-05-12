20ceac78ffedb0576bc264c277fd3ea4d3dd8471
SeongJoong <ksj960712@naver.com>
2023-05-12 18:06:13 +0900

    Test: QT app comm test

---
+++ QtCommTest/main/mqtt.cpp
@@ -0,0 +1,15 @@
+#include "mqtt.h"
+
+std::string NOW_PWD = "123456";
+
+void MqttCallback::connection_lost(const std::string &cause)
+{
+    std::cout << "Connection lost: " << cause << std::endl;
+}
+
+void MqttCallback::message_arrived(mqtt::const_message_ptr msg)
+{
+    std::string topic = msg->get_topic();
+    std::string data = msg->to_string();
+    std::cout << "'" << topic << "' : " << data << std::endl;
+}
