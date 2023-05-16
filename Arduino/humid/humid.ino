bbf83d410bef6b8c97d2c338244eac52d662fa3d
unknown <06___what@naver.com>
2023-05-16 10:40:26 +0900

    Feat: humid and waterpump function
    습도 측정및 워터 펌프 작동기능 구현
    
    Related to: #S08P31A206-330

--- Arduino/humid/humid.ino
+++ Arduino/humid/humid.ino
@@ -1,9 +1,8 @@
 #include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
 
- 
 bool flag = false;    // 현재 작동상태 기록
-bool order = false;   // 작동 명령 여부
-float now = 0;        // 현재 센싱값
+float now_sensing = 0;        // 현재 센싱값
 
 int sensor = 0;       // 토양습도센서
 int motor = 9;        // 워터펌프
@@ -13,52 +12,59 @@ int R_min = 0;
 int R_current = 500;
 int difference = R_max - R_current;
 
+int past = 0;       //  모터 작동을 위한 시간 기록
+int pasttime = 0;   // 10분마다의 전송을 위한 시간 기록 
 
-// 제어 함수
+
+// 모터 on
 void run_motor()
 {
-  Serial.print(" run_motor now!! ");
-  
+  Serial.print(" motor start!! ");
+  digitalWrite(motor, HIGH); 
+}
+
+// 모터 off
+void stop_motor()
+{
+  Serial.print(" motor stop!! ");
+  digitalWrite(motor, LOW);
 }
 
 // 송신함수 
 void SendMessage()
 {
   Serial.print(" SendMessage : ");
- 
 }
  
 void setup()
 {
   Serial.begin(9600);
   pinMode(motor,OUTPUT);      // 모터핀설정
-
-  
+  pasttime = millis();
 }
  
 void loop()
 {
   sensor = analogRead(A0);    // 센싱
-  now = (float)(1023 - sensor) / 1023 * 100;
-  //now = round(now * 10) / 10;
-  Serial.println(now);
+  now_sensing = (float)(1023 - sensor) / 1023 * 100;
+  Serial.println(now_sensing);
   delay(1000);
 
-
-  if (Serial.available()) { // 시리얼 버퍼에 데이터가 있는 경우 : 작동 명령
-  order = true;  
-  if(order){  // 작동 명령이 떨어지면
+  if (Serial.available()) { // 작동 명령 전달
     if(!flag){    // 미작동중 상태라면
       flag = true;    // 작동 플래그 처리
-
-
-
+      run_motor();
+      past  = millis();
     }
   }
 
-
+  if(millis() - past >= 20000){ // 20초 작동 후 종료
+    stop_motor();
+    flag = false;
   }
 
-
-  
+  if(millis() - pasttime >= 600000){  // 10분 마다 송신
+    SendMessage();
+    pasttime = millis();
+  } 
 }
