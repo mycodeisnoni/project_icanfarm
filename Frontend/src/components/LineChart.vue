f1f44bca8bf5a5696471fbafd9d315c74779b4ee
공한울 <gonoghanan@naver.com>
2023-05-18 17:47:22 +0900

    Feat: Add MonitorBoard Graph

--- Frontend/src/components/LineChart.vue
+++ Frontend/src/components/LineChart.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="chart-container" ref="container">
     <canvas ref="lineChart" height="70"></canvas>
   </div>
 </template>
@@ -10,6 +10,7 @@ import Chart from 'chart.js/auto';
 export default {
   name: 'LineChart',
   props: {
+    chart: null,
     chartData: {
       type: Object,
       required: true,
@@ -28,8 +29,24 @@ export default {
   mounted() {
     this.renderChart();
   },
+  beforeUpdate() {
+    // if (this.chart) {
+    //   this.chart.destroy();
+    //   this.chart = null;
+    // }
+    this.renderChart();
+  },
+  beforeDestroy() {
+    if (this.lineChart) {
+      this.lineChart.destroy();
+      this.chart = null;
+    }
+  },
   methods: {
     renderChart() {
+      if (this.chart) {
+        this.chart.destroy();
+      }
       const canvas = this.$refs.lineChart;
       this.lineChart = new Chart(canvas, {
         type: 'line',
@@ -38,10 +55,6 @@ export default {
       });
     },
   },
-  beforeDestroy() {
-    if (this.lineChart) {
-      this.lineChart.destroy();
-    }
-  },
+
 };
 </script>
